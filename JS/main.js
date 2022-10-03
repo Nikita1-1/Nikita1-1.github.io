025ec6791db67b5524deb13d74204a870ab9eef7
Nikita Sazonov <sazonovn2018@gmail.com>
2022-10-02 21:20:04 -0600

    commit

--- JS/main.js
+++ JS/main.js
@@ -39,7 +39,7 @@ function showSlides(n) {
 
 // This code will create close the contact form when the user clicks off of it
 // The first step is to add an event listener for any clicks on the website
-document.addEventListener("click", function(event){
+document.addEventListener("click", (event) =>{
     // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
     if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
         closeForm()
